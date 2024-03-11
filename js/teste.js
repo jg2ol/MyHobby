fea8a0b5585140d016df3ef5a2cb386d1177a5d6
jg2ol <159553250+jg2ol@users.noreply.github.com>
2024-03-10 21:13:23 -0300

    aula14css
    tocansado

---
+++ js/teste.js
@@ -0,0 +1,29 @@
+const escola = [
+    {
+    nome: "João",
+    sala: 39,
+    nota: 1 },
+    {
+    nome: "Vitor",
+    sala: 40,
+    nota: 2 },
+    {
+    nome: "Thiago",
+    sala: 41,
+    nota: 3 },
+    {
+    nome: "José",
+    sala: 42,
+    nota: 4 },
+    {
+    nome: "Sandro",
+    sala: 43,
+    nota: 5 }
+];
+console.log(escola[3]);
+
+function soma (a,b){return a+b};
+console.log("3+5 =", soma (3,5));
+
+for (let i=0; i<13; i++){console.log(i)};
+
