11dd447cdc62a0bd42ea39387815a96933908cde
jg2ol <159553250+jg2ol@users.noreply.github.com>
2024-03-16 16:37:51 -0300

    slide2
    chama

---
+++ js/video.js
@@ -0,0 +1,14 @@
+const modal_overlay = document.querySelector ('.modal_overlay');
+const cards = document.querySelectorAll ('.card');
+
+for (let card of cards){
+    card.addEventListener ("click", function(){
+        modal_overlay.classList.add('active')
+        modal_overlay.querySelector("iframe").src=`https://youtube.com/embed/${videoId}`;
+    })
+}
+
+document.querySelector ('close_modal').addEventListener("click",function(){
+    modal_overlay.classList.remove("active")
+    modal_overlay.querySelector("iframe").src="";
+})
