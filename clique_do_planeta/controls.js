85ff69e342fb23cea4a3816c519aa838b687da25
jg2ol <159553250+jg2ol@users.noreply.github.com>
2024-03-26 02:22:21 -0300

    cliquedoplanetafasesiniciaispt1
    sao02:22

---
+++ clique_do_planeta/controls.js
@@ -0,0 +1,9 @@
+const planeta_compras = document.getElementById('planeta_compras');
+const planeta = document.getElementById('planeta');
+const compras = document.getElementById('compras');
+
+function compras_maquinas() {
+    planeta.style.width = "60%";
+    planeta.style.transition = "450ms";
+    compras.style.display = "inline-block";
+}
